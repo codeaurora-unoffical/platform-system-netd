34aa5dfaf54ec62410316c0f0d353a261648ec10
Santosh Sajjan <ssajjan@codeaurora.org>
2011-10-31 11:32:13 +0530

    wlan: Adding license disclaimer for open source changes to support AR600X WLAN.
    Change-Id: I16aab958259fec27767a5b8fc525d3d736ed4f13

--- SoftapController.cpp
+++ SoftapController.cpp
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (c) 2011 Code Aurora Forum
+ * Not a Contribution. Notifications and licenses are retained for attribution purposes only
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
