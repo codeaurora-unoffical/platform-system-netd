482b0b39b93ee8c3ee56376465d7b6a53cbf8345
Santosh Sajjan <ssajjan@codeaurora.org>
2012-08-01 13:54:51 +0530

    netd: softap: Changes to support AR6004 softap.
    Add changes to run the corresponding modules based on
    runtime detection of ATH and WCN WLAN.
    
    Change-Id: I14a67799ad67afc60740a0ff783dc25f16f75eec

--- SoftapController.cpp
+++ SoftapController.cpp
@@ -190,7 +190,7 @@ int SoftapController::startSoftap() {
 
 #ifdef QCOM_WLAN
     property_get("wlan.driver.ath", ath6kl_supported, 0);
-    if (*ath6kl_supported != '1')
+    if (*ath6kl_supported != '1' && *ath6kl_supported != '2')
     {
        qsap_set_ini_filename();
     }
@@ -298,7 +298,7 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
     iface = argv[2];
 #ifdef QCOM_WLAN
     property_get("wlan.driver.ath", ath6kl_supported, 0);
-    if (*ath6kl_supported != '1')
+    if (*ath6kl_supported != '1' && *ath6kl_supported != '2')
     {
         return qsapsetSoftap(argc, argv);
     }
