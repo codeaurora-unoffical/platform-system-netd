445270ba1a0ea33468aa1461b8269e90e191395b
Jeff Johnson <jjohnson@codeaurora.org>
2013-01-15 10:41:53 -0800

    netd: Support the Qualcomm SoftAP SDK
    The Qualcomm SoftAP SDK extendes the HotSpot capability provided by
    the stock Android distribution.  This allows the creation of a more
    feature-rich HotSpot application.
    
    In order to minimize the changes required to the stock Android
    distribution, place the majority of the changes into a new
    QualcommSoftapCmd class that contains most of the command dispatching
    logic.
    
    Change-Id: I3dd1690095f81820bf1d712f07198bba5b13629e

--- Android.mk
+++ Android.mk
@@ -41,6 +41,13 @@ LOCAL_CFLAGS := -Werror=format
 LOCAL_SHARED_LIBRARIES := libstlport libsysutils libcutils libnetutils \
                           libcrypto libhardware_legacy libmdnssd libdl
 
+ifeq ($(BOARD_HAS_QCOM_WLAN), true)
+  LOCAL_SRC_FILES += QualcommSoftapCmd.cpp
+  LOCAL_CFLAGS += -DQCOM_WLAN
+  LOCAL_SHARED_LIBRARIES += libqsap_sdk
+  LOCAL_C_INCLUDES += $(LOCAL_PATH)/../qcom/softap/sdk/
+endif
+
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
