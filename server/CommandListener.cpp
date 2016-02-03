bcc4796ff0c851b15d5f273198216bb676a45e12
Pragnya Paramita <prgnya@codeaurora.org>
2016-02-03 18:04:35 +0530

    system/netd: Addition of DNS proxy block change for ZeroBalance.
     -- Addition of QtiDataController cpp and header file to
        write rules to block unblock 8.8.8.4 and 8.8.8.8 dns
        by writing rules to IPTable oem_out chain
     -- When zero balance is reached all dns queries are blocked
        except browser uid in DnsProxyListener file.
     -- All code changes are done inside a library under proprietary
        path and methods from library are used in
        QtiDataController.cpp
    
    Change-Id: Ia1022e2a524bd70175f5834f1e839a8ec4e40b47
    CRs-Fixed: 927258

--- server/CommandListener.cpp
+++ server/CommandListener.cpp
@@ -46,6 +46,7 @@
 #include "RouteController.h"
 #include "UidRanges.h"
 #include "QtiConnectivityAdapter.h"
+#include "QtiDataController.h"
 
 #ifdef QSAP_WLAN
 #include "qsap_api.h"
@@ -216,6 +217,7 @@ CommandListener::CommandListener() :
         sClatdCtrl = new ClatdController(sNetCtrl);
     if (!sStrictCtrl)
         sStrictCtrl = new StrictController();
+    initializeDataControllerLib();
 
     /*
      * This is the only time we touch top-level chains in iptables; controllers
@@ -1261,6 +1263,24 @@ int CommandListener::BandwidthControlCmd::runCommand(SocketClient *cli, int argc
             return 0;
         }
         int rc = sBandwidthCtrl->removeRestrictAppsOnWlan(argc - 2, argv + 2);
+        sendGenericOkFail(cli, rc);
+        return 0;
+    }
+    if (!strcmp(argv[1], "blockAllData")) {
+        if (argc < 2) {
+            sendGenericSyntaxError(cli, "zerobalanceblock");
+            return 0;
+        }
+        int rc = blockAllData();
+        sendGenericOkFail(cli, rc);
+        return 0;
+    }
+    if (!strcmp(argv[1], "unblockAllData")) {
+        if (argc < 2) {
+            sendGenericSyntaxError(cli, "zerobalance unblock");
+            return 0;
+        }
+        int rc = unblockAllData();
         sendGenericOkFail(cli, rc);
         return 0;
     }
